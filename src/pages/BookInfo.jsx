43ad823cb66e4709f96858c900828889bbf02edd
JamKyu <jamkyu@pm.me>
2022-07-15 01:02:40 -0400

    Updated BookInfo Row

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -21,49 +21,51 @@ const BookInfo = ({ books, addToCart, cart }) => {
     <div id="books__body">
       <main id="books__main">
         <div className="books__container">
-          <div className="book__selected--top">
-            <Link to="/books" className="book__link">
-              <FontAwesomeIcon icon="arrow-left" />
-            </Link>
-            <Link to="/books" className="book__link">
-              <h2 className="book__selected--title--top">Books</h2>
-            </Link>
-          </div>
-          <div className="book__selected">
-            <figure className="book__selected--figure">
-              <img src={book.url} alt="" className="book__selected--img" />
-            </figure>
-            <div className="book__selected--description">
-              <h2 className="book__selected--title">{book.title}</h2>
-              <Rating rating={book.rating} />
-              <div className="book__selected--price">
-                <Price
-                  salePrice={book.salePrice}
-                  originalPrice={book.originalPrice}
-                />
+          <div className="row">
+            <div className="book__selected--top">
+              <Link to="/books" className="book__link">
+                <FontAwesomeIcon icon="arrow-left" />
+              </Link>
+              <Link to="/books" className="book__link">
+                <h2 className="book__selected--title--top">Books</h2>
+              </Link>
+            </div>
+            <div className="book__selected">
+              <figure className="book__selected--figure">
+                <img src={book.url} alt="" className="book__selected--img" />
+              </figure>
+              <div className="book__selected--description">
+                <h2 className="book__selected--title">{book.title}</h2>
+                <Rating rating={book.rating} />
+                <div className="book__selected--price">
+                  <Price
+                    salePrice={book.salePrice}
+                    originalPrice={book.originalPrice}
+                  />
+                </div>
+                <div className="book__summary">
+                  <h3 className="book__summary--title">Summary</h3>
+                  <p className="book__summary--para">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Culpa nobis eveniet ratione nesciunt quisquam aperiam ipsa
+                    animi aut saepe dolorem.
+                  </p>
+                  <p className="book__summary--para">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Culpa nobis eveniet ratione nesciunt quisquam aperiam ipsa
+                    animi aut saepe dolorem.
+                  </p>
+                </div>
+                {bookExistsOnCart() ? (
+                  <Link to={`/cart`} className="book__link">
+                    <button className="btn">Checkout</button>
+                  </Link>
+                ) : (
+                  <button className="btn" onClick={() => addBookToCart(book)}>
+                    Add to Cart
+                  </button>
+                )}
               </div>
-              <div className="book__summary">
-                <h3 className="book__summary--title">Summary</h3>
-                <p className="book__summary--para">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Culpa nobis eveniet ratione nesciunt quisquam aperiam ipsa
-                  animi aut saepe dolorem.
-                </p>
-                <p className="book__summary--para">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Culpa nobis eveniet ratione nesciunt quisquam aperiam ipsa
-                  animi aut saepe dolorem.
-                </p>
-              </div>
-              {bookExistsOnCart() ? (
-                <Link to={`/cart`} className="book__link">
-                  <button className="btn">Checkout</button>
-                </Link>
-              ) : (
-                <button className="btn" onClick={() => addBookToCart(book)}>
-                  Add to Cart
-                </button>
-              )}
             </div>
           </div>
         </div>
